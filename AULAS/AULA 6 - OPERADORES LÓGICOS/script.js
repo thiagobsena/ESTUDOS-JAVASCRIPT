7ab283eb4e0cd4e8d0828d1af63a27da2d7f7a2f
Thiago Sena <thiago.bsenasantos@gmail.com>
2023-04-25 23:21:11 -0300

    Operadores lógicos

---
+++ AULAS/AULA 6 - OPERADORES LÓGICOS/script.js
@@ -0,0 +1,33 @@
+// && - SIGNIFIACA: E
+// || SIGNIFICA: OU
+// NEGATIVO
+
+
+// var a = false;
+// var b = false;
+
+// var c = a && b;
+
+// console.log(c);
+
+var idade = 25;
+
+var maior20 = idade > 20;
+var menor30 = idade < 30;
+
+var entre = maior20 && menor30;
+
+console.log("idade", idade);
+
+console.log("Maior que vinte", maior 20);
+
+
+// NEGATIVO
+
+var idade = 35;
+
+var maior20 = idade >=20;
+var menor20 = !maior20;
+
+console.log("Maior que vinte", maior20);
+console.log("Menor que vinte", menor20);
